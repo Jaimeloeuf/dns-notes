1c5038e8862f6fd16b960316b5a84be13995bb6c
Jaime <jaimeloeuf@gmail.com>
2022-01-22 22:45:39 +0800

    Update context menus: 1.  Update context menus. 2.  Add new context menu for selections only. 3.  Move context menu creation into onInstalled event listener so it     only runs once on install instead of running everytime background     script is triggered.

--- src/background.js
+++ src/background.js
@@ -2,16 +2,27 @@ chrome.runtime.onInstalled.addListener((installationObject) => {
   if (installationObject.reason === chrome.runtime.OnInstalledReason.INSTALL) {
     console.log("DNS-Notes installed");
   }
-});
 
-chrome.contextMenus.create({
-  id: "context-menu-create",
-  title: "Create new note",
-});
+  chrome.contextMenus.create({
+    id: "cm-create",
+    title: "Create new note for '%s'",
+    type: "normal",
+    contexts: ["selection"],
+  });
 
-chrome.contextMenus.create({
-  id: "context-menu-view",
-  title: "View notes",
+  chrome.contextMenus.create({
+    id: "cm-view",
+    title: "View notes for '%s'",
+    type: "normal",
+    contexts: ["selection"],
+  });
+
+  chrome.contextMenus.create({
+    id: "cm-view-all",
+    title: "View notes for current domain",
+    type: "normal",
+    contexts: ["page"],
+  });
 });
 
 chrome.contextMenus.onClicked.addListener(async function (info, tab) {
