ad672257ec14b6fe3ea3646f5c2c3e585c7d0cbb
Jaime <jaimeloeuf@gmail.com>
2022-02-10 15:31:44 +0800

    Add note sync API for clients to post events to.

--- server/src/routes/note.js
+++ server/src/routes/note.js
@@ -54,4 +54,82 @@ router.get(
   )
 );
 
+/**
+ * API for client to send events to
+ * @name POST /note/sync/:orgID
+ */
+router.post(
+  "/sync/:orgID",
+
+  authzMW((token, req) => req.params.orgID === token.org),
+
+  // Only parse request body if user authorized to access API
+  express.json(),
+
+  // Middleware to ensure event object is available
+  (req, res, next) => {
+    if (!req.body.event)
+      res.status(400).json({ error: "Missing event object" });
+    else next();
+  },
+
+  asyncWrap(async (req, res) => {
+    const { event } = req.body;
+
+    // Set a time field on the event object directly based on server's current time to sort events by time
+    // Although a note object already have a time field, it is time of note creation,
+    // This time field is for time of sync, so if there are 2 conflicting events, they are ordered by time of sync
+    /*
+      @todo
+      An alternative is to use firestore's time 'field value' instead of rolling with my own timestamp
+      Which might be even easier for firestore to order by.
+      However, it would be more platform dependent and be harder to experiment with.
+    */
+    event.time = unixseconds();
+
+    // Record who created this sync event and what organization is it for
+    event.user = req.authenticatedUser.email;
+    event.org = req.authenticatedUser.org;
+
+    switch (event.type) {
+      case "add":
+        const noteRef = fs.collection("notes").doc();
+        const { id } = noteRef;
+        event.note.id = id;
+        await noteRef.set(event.note);
+
+        // Add event into the event store
+        await fs.collection("events").add(event);
+
+        return res.status(201).json({ id });
+
+      case "del":
+        // Delete note from notes store
+        await fs.collection("notes").doc(event.noteID).delete();
+
+        // Add event into the event store
+        await fs.collection("events").add(event);
+
+        return res.status(200).json({});
+
+      case "edit":
+        // Update note in notes collection
+        await fs
+          .collection("notes")
+          .doc(event.note.id)
+          .update({ ...event.note });
+
+        // Add event into the event store
+        await fs.collection("events").add(event);
+
+        return res.status(200).json({});
+
+      default:
+        return res.status(400).json({
+          error: `Invalid event type '${event.type}' sent to sync API`,
+        });
+    }
+  })
+);
+
 module.exports = router;
