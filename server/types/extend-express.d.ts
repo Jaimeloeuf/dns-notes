d2f7657bc6f053521c506392fcefc7142a48c395
Jaime <jaimeloeuf@gmail.com>
2022-02-26 23:49:08 +0800

    Extend express's Request object type definition: 1.  Update tsconfig.json to include seperate types/ folder for seperate     project specific type definitions. 2.  Add type definition file to extend express's Request object type to     include `authenticatedUser` property set by firebase auth middleware.

---
+++ server/types/extend-express.d.ts
@@ -0,0 +1,23 @@
+import type express from "express";
+
+declare global {
+  namespace Express {
+    // Extending the Request type to add `authenticatedUser` property set by the firebase auth middleware library
+    interface Request {
+      // Possibly undefined if request is typed before using the `authn` middleware
+      // However almost all API routes are mounted after the authn middleware,
+      // So can safely assume that Request objects have the `authenticatedUser` property
+      // authenticatedUser?: Record<string, any>;
+
+      // Could be a generic Record type but since the set of properties and custom claims expected,
+      // is more or less fixed, they are manually typed out instead
+      authenticatedUser: {
+        email: string;
+        org: string;
+
+        // Admin Boolean is only set when true, else not set to reduce JWT size
+        admin: true | undefined;
+      };
+    }
+  }
+}
