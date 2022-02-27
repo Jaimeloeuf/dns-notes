ee77ffe9663725f9b90dafa68bfd5a3fc3670b79
Jaime <jaimeloeuf@gmail.com>
2022-02-27 15:00:51 +0800

    Add request body validation to note API:     Add checks for fields on the note object when a new note is synced     with the API to ensure that user cannot tamper with credentials/data.

--- server/src/routes/note.js
+++ server/src/routes/note.js
@@ -10,7 +10,7 @@ const router = express.Router();
 const unixseconds = require("unixseconds");
 const { asyncWrap } = require("express-error-middlewares");
 const authzMW = require("firebase-auth-express-middleware").authz;
-const fs = require("@enkeldigital/firebase-admin").fs;
+const { fs } = require("@enkeldigital/firebase-admin");
 
 /**
  * API to get all notes of an org as an object
@@ -109,6 +109,22 @@ router.post(
             .status(400)
             .json({ error: "Missing note object in event object" });
 
+        // Prevent users from setting other user's email as note creator
+        if (event.note.user !== req.authenticatedUser.email)
+          return res
+            .status(400)
+            .json({ error: "Notes must be created with your own email" });
+
+        // Prevent users from creating note under other organizations
+        if (event.note.org !== req.authenticatedUser.org)
+          return res
+            .status(400)
+            .json({ error: "Notes must belong to your own organization" });
+
+        // Setting this in the API so clients dont have to set it, and they cant change this field maliciously
+        event.note.user = req.authenticatedUser.email;
+        event.note.org = req.authenticatedUser.org;
+
         // Add note into the notes store with the firestore doc ID as the id field on the note object
         // Then get back the updated note object with ID to set on the event object,
         // So that the note object on the event object is the same as the note object stored in firestore.
@@ -143,6 +159,7 @@ router.post(
           .json(await getEvents(req.params.orgID, req.params.lastSync));
 
       case "edit":
+        // @todo Check to ensure that user did not change the id of the note??? maybe should have a seperate noteID field?
         // Update note in notes collection by overwriting the original note
         // Semantically similar to deleting and creating new one with the same note ID
         await fs.collection("notes").doc(event.note.id).set(event.note);
