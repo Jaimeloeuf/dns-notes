22c0ab9562eb0adaa036e5d622335810fd2ca7e5
Jaime <jaimeloeuf@gmail.com>
2022-02-28 17:37:03 +0800

    WIP Add org type definition and README.

---
+++ shared-types/README.md
@@ -0,0 +1,2 @@
+# Shared Types
+Shared type definition files used by both frontend and backend to enforce consistency when either of them requires a change in type.
---
+++ shared-types/org.d.ts
@@ -0,0 +1,10 @@
+export type Org = {
+  // Globally unique org ID for every org
+  id: string;
+
+  // Name of the organization
+  name: string;
+
+  // @todo Might do something else over here
+  // plan: 1 | 2 | 3;
+};
